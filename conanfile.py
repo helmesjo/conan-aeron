87dfae342507bbcb4cad132a40fe1ab0d7e96a1e
helmesjo <helmesjo@gmail.com>
2020-05-20 12:08:49 +0200

    Upped to v1.27.0

--- conanfile.py
+++ conanfile.py
@@ -24,7 +24,7 @@ def replace(file, pattern, subst):
 
 class LibnameConan(ConanFile):
     name = "aeron"
-    version = "1.21.0"
+    version = "1.27.0"
     description =   "Efficient reliable UDP unicast, \
                     UDP multicast, and IPC message transport"
     url = "https://github.com/real-logic/aeron"
